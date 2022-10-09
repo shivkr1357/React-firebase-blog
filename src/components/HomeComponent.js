b1551ebf0716250e34e5e679788f68b1e83729dc
Shiv Shankar Prasad <shivshankarkumar.pusa@gmail.com>
2022-10-09 06:34:01 +0530

    Fixed the dependency array

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -22,77 +22,28 @@ import SmartText from "../helpers/SmartText";
 import { data } from "../helpers/data";
 
 const HomeComponent = () => {
-  // const [postList, setPostList] = useState([]);
+  const [postList, setPostList] = useState([]);
 
-  // const postCollectionRef = collection(db, "posts");
+  const postCollectionRef = collection(db, "posts");
 
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const getPosts = async () => {
-  //     const data = await getDocs(postCollectionRef);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
 
-  //     setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  //   };
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-  //   getPosts();
-  // }, [postCollectionRef]);
+    // console.log(data);
+    getPosts();
+  }, []);
 
   return (
     <Box
       flex={4}
       sx={{ padding: { xs: "0", sm: "0px 20px ", marginTop: "10px" } }}>
-      <Card sx={{ marginBottom: "20px" }}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-              {Array.from("Hello World")[0]}
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={"Hello This is a title for the "}
-          subheader={data.date}
-          onClick={() => {
-            navigate("");
-          }}
-          sx={{ cursor: "pointer" }}
-        />
-        {/* <CardMedia
-          component="img"
-          height="20%"
-          // image={}
-          alt="Paella dish"
-        /> */}
-        <CardContent>
-          <SmartText text={data.desc} />
-          {/* <Typography variant="body2" color="text.secondary">
-              {post.desc}
-            </Typography> */}
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <Checkbox
-              icon={<FavoriteBorder />}
-              checkedIcon={<Favorite sx={{ color: "red" }} />}
-            />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          {/*  <ExpandMore
-            expand={expanded}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore> */}
-        </CardActions>
-      </Card>
-      {/* {postList.map((post, key) => {
+      {postList.map((post, key) => {
         return (
           <Card sx={{ marginBottom: "20px" }}>
             <CardHeader
@@ -122,8 +73,8 @@ const HomeComponent = () => {
             <CardContent>
               <SmartText text={post.desc} />
               {/* <Typography variant="body2" color="text.secondary">
-              {post.desc}
-            </Typography> 
+                {post.desc}
+              </Typography> */}
             </CardContent>
             <CardActions disableSpacing>
               <IconButton aria-label="add to favorites">
@@ -141,11 +92,11 @@ const HomeComponent = () => {
             aria-label="show more"
           >
             <ExpandMoreIcon />
-          </ExpandMore> 
+          {/* </ExpandMore>  */}
             </CardActions>
           </Card>
         );
-       */}
+      })}
     </Box>
   );
 };
